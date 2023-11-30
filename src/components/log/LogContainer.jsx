1304a81aec1daea523ec04abb9b0a9082609f4f5
caugsorn <caugsorn@gmail.com>
2023-12-01 01:51:35 +0700

    format

--- src/components/log/LogContainer.jsx
+++ src/components/log/LogContainer.jsx
@@ -1,4 +1,4 @@
-import {useContext, useState, useEffect} from "react";
+import { useContext, useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import LogsFilter from "./cards/LogsFilter";
 import Logcard from "./cards/Logcard";
@@ -7,52 +7,53 @@ import { LogContext } from "../../contexts/LogContext";
 
 export default function LogContainer() {
   const ctx = useContext(LogContext);
-  const param = useParams()
-      const [filterCategory, setFilterCategory] = useState({})
-      useEffect(()=> {
-        const categoryKeys = Object.keys(ctx.categoryByDate)
-        const categoryKeysObj = categoryKeys.reduce((acc, curr) =>  {
-          acc[curr] = true
-          return acc
-        }, {})
-        setFilterCategory(categoryKeysObj)
-    },[ctx.categoryByDate])
+  const param = useParams();
+  const [filterCategory, setFilterCategory] = useState({});
+  useEffect(() => {
+    const categoryKeys = Object.keys(ctx.categoryByDate);
+    const categoryKeysObj = categoryKeys.reduce((acc, curr) => {
+      acc[curr] = true;
+      return acc;
+    }, {});
+    setFilterCategory(categoryKeysObj);
+  }, [ctx.categoryByDate]);
 
+  const allLog = Object.values(ctx.logByDate);
 
-  const allLog = Object.values(ctx.logByDate)
-  
- 
   return (
-    <div className="mx-8 space-y-4 mt-12 h-11/12 w-11/12">
-        <div className="card row-start-1 col-span-full h-20">
+    <div className="mx-8 space-y-4 h-11/12 w-11/12">
+      <div className="card row-start-1 col-span-full h-20">
         <LogPagination />
       </div>
 
-      <div className="grid grid-cols-3 space-x-4 w-full h-3/5">
+      <div className="grid grid-cols-3 space-x-4 w-full h-full">
         <div className="card pl-10 pt-2">
-            <h3 className="mt-4 mb-1">Filter</h3>
-           <ul>
-           {/* {console.log(filterCategory)} */}
-            {Object.keys(ctx.categoryByDate).map((el) => { 
-              return <LogsFilter key={el} name={el} setFilterCategory={setFilterCategory} filterCategory={filterCategory}/>}
-            )}
-
-          </ul> 
+          <h3 className="mt-4 mb-1">Filter</h3>
+          <ul>
+            {/* {console.log(filterCategory)} */}
+            {Object.keys(ctx.categoryByDate).map((el) => {
+              return (
+                <LogsFilter
+                  key={el}
+                  name={el}
+                  setFilterCategory={setFilterCategory}
+                  filterCategory={filterCategory}
+                />
+              );
+            })}
+          </ul>
         </div>
-         <div className="card col-span-2 h-full overflow-y-auto">
-        {/* {Object.entries(ctx.logByDate).map((element,idx) => { 
-          console.log(element)
-          // element.filter(()=> filterCategory[element.cate])  
-        return <Logcard key={idx} element={element[1]}/>
-        })} */}
-         {(Object.values(ctx.logByDate).map((element,idx) => <Logcard key={element} element={element} filterCategory={filterCategory}/>))}
-
+        <div className="card col-span-2 h-full overflow-y-auto">
+          {Object.values(ctx.logByDate).map((element, idx) => (
+            <Logcard
+              key={element}
+              element={element}
+              filterCategory={filterCategory}
+            />
+          ))}
+        </div>
+        {/* </div> */}
       </div>
-      {/* </div> */}
-     
-      </div>
-
-      
     </div>
   );
 }
